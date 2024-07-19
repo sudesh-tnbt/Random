f55b4b91010c337e9724dea35edd21e0f39a984d
sudesh.tnbt <sudeshacharya110@gmail.com>
2024-07-19 12:21:57 +0545

    Supports 10 random numbers from seed = 42

--- MSM/random.js
+++ MSM/random.js
@@ -1,12 +1,14 @@
-let seed = 10;
+let seed = 42
 
 function msm() {
 
-    let result = (seed ^ 2).toString().slice(1, 3)
+    let result = (seed * seed).toString().slice(1, 3)
 
     seed = parseInt(result)
 
     return parseInt(result)
 }
 
-console.log(msm())
+for (let i = 0; i < 10; i++) {
+    console.log(msm())
+}
